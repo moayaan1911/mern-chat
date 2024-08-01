1916430e11cd1001ba38eaca558466367052653a
moayaan1911 <ayaangames@gmail.com>
2024-08-02 03:01:43 +0530

    user model completed

---
+++ backend/models/user.model.ts
@@ -0,0 +1,38 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: 8,
+    },
+    gender: {
+      type: String,
+      required: true,
+      enum: ['male', 'female', 'other'],
+    },
+    profilePic: {
+      type: String,
+      default: '',
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model('User', userSchema);
